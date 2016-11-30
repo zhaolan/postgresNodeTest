2478a0457ed0362b8290f22a368516683be3bb89
zhaolan <lanzhao@thoughtworks.com>
2016-11-30 19:24:23 +0800

    fix the connect

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post('/title/:name', jsonParser, function (req, res) {
   })
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
